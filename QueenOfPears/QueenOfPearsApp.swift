774cfade55fc69da555afb1b45edc4cf30cffb02
Vina Melody <vina_melody@yahoo.com>
2024-07-07 20:02:01 +0800

    Initial Commit

---
+++ QueenOfPears/QueenOfPearsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueenOfPearsApp.swift
+//  QueenOfPears
+//
+//  Created by Vina Melody on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct QueenOfPearsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QueenOfPearsTests/QueenOfPearsTests.swift
@@ -0,0 +1,17 @@
+//
+//  QueenOfPearsTests.swift
+//  QueenOfPearsTests
+//
+//  Created by Vina Melody on 7/7/24.
+//
+
+import Testing
+@testable import QueenOfPears
+
+struct QueenOfPearsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
